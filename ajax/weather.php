ff5d15d485974b2d735babc4aed1fc3a8c77a32b
hunger <hunger@jirengu.com>
2015-10-14 14:43:05 +0800

    up

---
+++ ajax/weather.php
@@ -0,0 +1,20 @@
+<?php
+	set_time_limit(0);
+	$private_key = '1af25d_SmartWeatherAPI_575592d';
+	$appid='75988d93774b5659';
+	$appid_six=substr($appid,0,6);
+	$areaid = '101010100';  
+	$type='forecast_v';
+	$date=date("YmdHi");
+	$public_key="http://open.weather.com.cn/data/?areaid=".$areaid."&type=".$type."&date=".$date."&appid=".$appid;
+	 
+	$key = base64_encode(hash_hmac('sha1',$public_key,$private_key,TRUE));
+	 
+	$URL="http://open.weather.com.cn/data/?areaid=".$areaid."&type=".$type."&date=".$date."&appid=".$appid_six."&key=".urlencode($key);
+	echo $URL."<br /-->";
+	 
+	$string=file_get_contents($URL);
+	 
+	echo $string;
+ 
+?>
