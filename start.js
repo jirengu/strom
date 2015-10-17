b3b259dc1ad044b3582cd05405b27e56c4572847
hunger <hunger@jirengu.com>
2015-10-17 17:37:18 +0800

    modify

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 var process = require('child_process');
 
 
-process.exec(' git add .; git commit -am "modify"; git push', function (error, stdout, stderr) {
+process.exec(' git add .; git commit -am "modify"; git push; git push -u sae master:1;', function (error, stdout, stderr) {
 	console.log('exec:  upload');
     if (error !== null) {
       console.log('exec error: ' + error);
