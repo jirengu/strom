4997137191d043ec661db867ea623b7420005049
hunger <hunger@jirengu.com>
2015-10-17 18:12:27 +0800

    modify

--- ajax/weather.php
+++ ajax/weather.php
@@ -34,7 +34,7 @@ function getCity(){
 	$str = file_get_contents($uri);
 	//echo $str;
 	$cityArr = json_encode($str);
-	echo $cityArr['content']['city'];
+	echo $cityArr['content']['address_detail']['city'];
 }
 
 function getWeather2(){
