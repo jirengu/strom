1e156375294e76c43df11b542ec52b24bac3d32e
hunger <hunger@jirengu.com>
2015-10-17 18:32:00 +0800

    modify

--- ajax/weather.php
+++ ajax/weather.php
@@ -42,7 +42,7 @@ function getCity(){
 }
 
 function getWeather2(){
-	$city = '杭州市';
+	$city = getCity();
 	$uri = "http://api.map.baidu.com/telematics/v3/weather?location=$city&output=json&ak=A20cb515b0fa284cb99c36956c06e737";
 	$str = file_get_contents(($uri));
 	echo $str;
