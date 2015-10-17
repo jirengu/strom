7010c7a492c65ff969552bc29b1fab5f8e6e5b41
hunger <hunger@jirengu.com>
2015-10-17 18:28:06 +0800

    modify

--- ajax/weather.php
+++ ajax/weather.php
@@ -72,7 +72,7 @@ function getWeather2(){
 	    }
 	    echo $realip;
 	    if(strpos($realip, ',') >= 0){
-	    	echo 1;
+	    	echo strpos($realip, ',');
 	    	$realip = substr($realip , 0, strpos($realip, ',')+1);
 	    	echo $realip;
 	    }
